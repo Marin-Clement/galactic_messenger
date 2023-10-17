f89c92377f41a46328aaeb23604912102a821d90
clementmarin <clement.marin@laplateforme.io>
2023-10-17 15:33:34 +0200

    [ADD] Connection + register + messages

--- src/main/java/server/controller/MessageController.java
+++ src/main/java/server/controller/MessageController.java
@@ -1,16 +1,33 @@
 package server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import server.component.UserSessionManager;
+import server.messaging.PrivateMessage;
 
 @Controller
 public class MessageController {
+    private UserSessionManager userSessionManager;
+
+    @Autowired
+    public MessageController(UserSessionManager userSessionManager) {
+        this.userSessionManager = userSessionManager;
+    }
 
     @MessageMapping("/send-message")
     @SendTo("/topic/messages")
-    public String sendMessage(String message) {
-        System.out.println("Received message: " + message);
-        return message;
+    public PrivateMessage sendMessage(PrivateMessage message) {
+        String sender = message.getSender();
+
+        // Check if the sender is a connected user
+        if (userSessionManager.isUserConnected(sender)) {
+            System.out.println("Received message: " + message.getContent() + " from " + sender + " to " + message.getRecipient());
+            return message;
+        } else {
+            System.out.println("Received message from an unconnected user: " + message.getContent() + " to " + "(" + message.getRecipient() + ")");
+            return null;
+        }
     }
 }
