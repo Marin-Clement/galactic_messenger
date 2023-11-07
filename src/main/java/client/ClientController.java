4cbeaef5a57147f2fc0311554e3439a7d23eb5cc
elie.aigon <elie.aigon@laplateforme.io>
2023-11-07 10:55:52 +0100

    commit

---
+++ src/main/java/client/ClientController.java
@@ -0,0 +1,215 @@
+package client;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.stomp.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+import server.messaging.PrivateMessage;
+
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class ClientController {
+    private StompSession stompSession;
+    String username = "Not logged in";
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private final int port = 8080;
+
+    private String serverUrl = "http://localhost:" + port + "/api/";
+
+    public static void main(String[] args) {
+        ClientController client = new ClientController();
+        client.connectAndRecieve();
+    }
+    public void connectAndRecieve() {
+//        performLoginOrRegister();
+        WebSocketStompClient stompClient = new WebSocketStompClient(new SockJsClient(
+                Collections.singletonList(new WebSocketTransport(new StandardWebSocketClient()))));
+
+        StompSessionHandler sessionHandler = new MyStompSessionHandler();
+        StompSession stompSession;
+        try {
+            stompSession = stompClient.connectAsync("ws://localhost:" + port + "/ws", sessionHandler).get();
+
+            // Subscribe to the /topic/PrivateMessages to receive PrivateMessages from the server
+            stompSession.subscribe("/topic/messages/" + username, new StompFrameHandler() {
+                @Override
+                public Type getPayloadType(StompHeaders headers) {
+                    return byte[].class; // Specify the payload type as byte[]
+                }
+
+                @Override
+                public void handleFrame(StompHeaders headers, Object payload) {
+                    byte[] PrivateMessageBytes = (byte[]) payload;
+                    PrivateMessage PrivateMessage = deserializePrivateMessage(PrivateMessageBytes);
+                    // if the PrivateMessage is from the user, don't print it
+                    if (!PrivateMessage.getSender().equals(username) && PrivateMessage.getRecipient().equals(username)) {
+                        System.out.println("Received PrivateMessage: " + PrivateMessage.getContent() + " from " + PrivateMessage.getSender());
+                    }
+                }
+            });
+
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String recipient;
+                System.out.println("Enter a PrivateMessage to send (or type 'exit' to quit): ");
+                String input = scanner.nextLine();
+                System.out.println("Enter the recipient: ");
+                recipient = scanner.nextLine();
+                if (input.equalsIgnoreCase("exit")) {
+                    break;
+                }
+                PrivateMessage PrivateMessage = new PrivateMessage(username, recipient, input);
+                byte[] PrivateMessageBytes = serializePrivateMessage(PrivateMessage);
+                stompSession.send("/app/send-PrivateMessage", PrivateMessageBytes);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class MyStompSessionHandler extends StompSessionHandlerAdapter {
+        @Override
+        public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+            System.out.println("Connected to WebSocket Server");
+        }
+    }
+
+    private void performLoginOrRegister() {
+        Scanner scanner = new Scanner(System.in);
+
+        // Perform login or register based on user input
+        while (true) {
+            System.out.println("Login or Register? (or type 'anonymous' to continue as anonymous user)");
+            String choice = scanner.nextLine();
+
+            if ("login".equalsIgnoreCase(choice)) {
+                System.out.println("Enter your username: ");
+                String username = scanner.nextLine();
+                System.out.println("Enter your password: ");
+                String password = scanner.nextLine();
+                login(username, password);
+                this.username = username;
+                break;
+            } else if ("register".equalsIgnoreCase(choice)) {
+                System.out.println("Enter a new username: ");
+                String username = scanner.nextLine();
+                System.out.println("Enter a password: ");
+                String password = scanner.nextLine();
+                register(username, password);
+                this.username = username;
+                login(username, password);
+                break;
+            } else if ("anonymous".equalsIgnoreCase(choice)) {
+                this.username = "anonymous";
+                break;
+            } else {
+                System.out.println("Invalid choice.");
+            }
+        }
+    }
+
+    private void login(String username, String password) {
+        String loginUrl = serverUrl + "login?username=" + username + "&password=" + password;
+        ResponseEntity<String> responseEntity = restTemplate.exchange(loginUrl, HttpMethod.POST, null, String.class);
+        System.out.println(responseEntity.getBody());
+    }
+
+    private void register(String username, String password) {
+        String registerUrl = serverUrl + "register?username=" + username + "&password=" + password;
+        ResponseEntity<String> responseEntity = restTemplate.exchange(registerUrl, HttpMethod.POST, null, String.class);
+        System.out.println(responseEntity.getBody());
+    }
+    private void CommandsInterface(){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("What do you wan't to do do ? : ");
+            String command = scanner.nextLine();
+            String[] commandSplit = command.split(" ");
+            switch (command){
+                case "/help" ->help();
+                case "/private_chat" -> private_chat(commandSplit[1]);
+                case "/create_group" -> create_group(commandSplit[1], false);
+                case "/join_group" -> join_group(commandSplit[1], false);
+                case "/msg_group" -> msg_group(commandSplit[1], commandSplit[2]);
+                case "/create_secure_group" -> create_group(commandSplit[1], true);
+                case "/join_secure_group" -> join_group(commandSplit[1], true);
+                case "/online_users" -> online_users();
+            }
+        }
+    }
+
+    private void help(){
+
+    }
+
+    private void online_users(){
+
+    }
+
+    private void private_chat(String recipient){
+
+    }
+    private void accept(){
+
+    }
+    private void decline(){
+
+    }
+    private void create_group(String group_name, boolean isSecure){
+        String url;
+        if(isSecure){
+            url = serverUrl + "create-group?groupName=" + group_name + "&isSecure=" + isSecure;
+        }else{
+            url = serverUrl + "create-group?groupName=" + group_name;
+        }
+        ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, null, String.class);
+    }
+    private void join_group(String group_name, boolean isSecure){
+
+    }
+    private void msg_group(String group_name, String content){
+
+    }
+
+
+    private void SendPrivatePrivateMessage(String recipient, String content){
+        PrivateMessage PrivateMessage = new PrivateMessage(username, recipient, content);
+        byte[] bytesPrivateMessage = serializePrivateMessage(PrivateMessage);
+        stompSession.send("/app/send-PrivateMessage/" + recipient, bytesPrivateMessage);
+    }
+
+    private void SendGroupPrivateMessage(String groupName, String content){
+        PrivateMessage PrivateMessage = new PrivateMessage(username, groupName, content);
+        byte[] bytesPrivateMessage = serializePrivateMessage(PrivateMessage);
+        stompSession.send("/app/send-group-PrivateMessage/" + groupName, bytesPrivateMessage);
+    }
+
+    private byte[] serializePrivateMessage(PrivateMessage PrivateMessage) {
+        try {
+            return objectMapper.writeValueAsBytes(PrivateMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new byte[0];
+        }
+    }
+
+    private PrivateMessage deserializePrivateMessage(byte[] PrivateMessageBytes) {
+        try {
+            return objectMapper.readValue(PrivateMessageBytes, PrivateMessage.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new PrivateMessage("", "", "");
+        }
+    }
+}
--- src/main/java/client/WebSocketClient.java
+++ src/main/java/client/WebSocketClient.java
@@ -1,20 +1,18 @@
 package client;
 
+import ch.qos.logback.core.net.server.Client;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.*;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.NonNullApi;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.messaging.simp.stomp.*;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
-
+import server.messaging.PrivateMessage;
 import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.Scanner;
-import server.messaging.PrivateMessage;
 
 
 public class WebSocketClient {
@@ -27,127 +25,7 @@ public class WebSocketClient {
     private final String serverUrl = "http://localhost:" + port + "/api/";
 
     public static void main(String[] args) {
-        WebSocketClient client = new WebSocketClient();
-        client.connectAndSend();
-    }
-
-    public void connectAndSend() {
-        performLoginOrRegister();
-        WebSocketStompClient stompClient = new WebSocketStompClient(new SockJsClient(
-                Collections.singletonList(new WebSocketTransport(new StandardWebSocketClient()))));
-
-        StompSessionHandler sessionHandler = new MyStompSessionHandler();
-        StompSession stompSession;
-        try {
-            stompSession = stompClient.connect("ws://localhost:" + port + "/messages", sessionHandler).get();
-            System.out.println("Connected to WebSocket Server");
-
-            // Subscribe to the /topic/messages to receive messages from the server
-            stompSession.subscribe("/topic/messages/" + username, new StompFrameHandler() {
-                @Override
-                public Type getPayloadType(StompHeaders headers) {
-                    return byte[].class; // Specify the payload type as byte[]
-                }
-
-                @Override
-                public void handleFrame(StompHeaders headers, Object payload) {
-                    byte[] messageBytes = (byte[]) payload;
-                    PrivateMessage message = deserializeMessage(messageBytes);
-                    // if the message is from the user, don't print it
-                    if (!message.getSender().equals(username) && message.getRecipient().equals(username)) {
-                        System.out.println("Received message: " + message.getContent() + " from " + message.getSender());
-                    }
-                }
-            });
-
-            Scanner scanner = new Scanner(System.in);
-            while (true) {
-                String recipient;
-                System.out.println("Enter a message to send (or type 'exit' to quit): ");
-                String input = scanner.nextLine();
-                System.out.println("Enter the recipient: ");
-                recipient = scanner.nextLine();
-                if (input.equalsIgnoreCase("exit")) {
-                    break;
-                }
-                PrivateMessage message = new PrivateMessage(username, recipient, input);
-                byte[] messageBytes = serializeMessage(message);
-                stompSession.send("/app/send-message", messageBytes);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void performLoginOrRegister() {
-        Scanner scanner = new Scanner(System.in);
-
-        // Perform login or register based on user input
-        while (true) {
-            System.out.println("Login or Register? (or type 'anonymous' to continue as anonymous user)");
-            String choice = scanner.nextLine();
-
-            if ("login".equalsIgnoreCase(choice)) {
-                System.out.println("Enter your username: ");
-                String username = scanner.nextLine();
-                System.out.println("Enter your password: ");
-                String password = scanner.nextLine();
-                login(username, password);
-                this.username = username;
-                break;
-            } else if ("register".equalsIgnoreCase(choice)) {
-                System.out.println("Enter a new username: ");
-                String username = scanner.nextLine();
-                System.out.println("Enter a password: ");
-                String password = scanner.nextLine();
-                register(username, password);
-                this.username = username;
-                login(username, password);
-                break;
-            } else if ("anonymous".equalsIgnoreCase(choice)) {
-                this.username = "anonymous";
-                break;
-            } else {
-                System.out.println("Invalid choice.");
-            }
-        }
-    }
-
-    private void login(String username, String password) {
-        String loginUrl = serverUrl + "login?username=" + username + "&password=" + password;
-        ResponseEntity<String> responseEntity = restTemplate.exchange(loginUrl, HttpMethod.POST, null, String.class);
-        System.out.println(responseEntity.getBody());
-    }
-
-    private void register(String username, String password) {
-        String registerUrl = serverUrl + "register?username=" + username + "&password=" + password;
-        ResponseEntity<String> responseEntity = restTemplate.exchange(registerUrl, HttpMethod.POST, null, String.class);
-        System.out.println(responseEntity.getBody());
-    }
-
-    private static class MyStompSessionHandler extends StompSessionHandlerAdapter {
-        @Override
-        public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
-            System.out.println("Connected to WebSocket Server");
-        }
-    }
-
-    // Serialize a PrivateMessage into bytes
-    private byte[] serializeMessage(PrivateMessage message) {
-        try {
-            return objectMapper.writeValueAsBytes(message);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new byte[0];
-        }
-    }
-
-    private PrivateMessage deserializeMessage(byte[] messageBytes) {
-        try {
-            return objectMapper.readValue(messageBytes, PrivateMessage.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new PrivateMessage("", "", "");
-        }
+        ClientController cc = new ClientController();
+        cc.connectAndRecieve();
     }
 }
